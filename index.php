c932f3d47ce905df9164102ea7c25b577309b084
Карпов Александр <karpoffSasha03@gmail.com>
2025-01-10 15:43:57 +0500

    less7

--- index.php
+++ index.php
@@ -4,11 +4,15 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use App\Controllers\CommentsController;
+use App\Controllers\LikesController;
 use App\Controllers\PostsController;
 
 $app = AppFactory::create();
 
 $app->post('/posts/comment', [CommentsController::class, 'createComment']);
 $app->delete('/posts', [PostsController::class, 'deletePost']);
+
+$app->post('/posts/like', [LikesController::class, 'createPostLike']);
+$app->post('/comments/like', [LikesController::class, 'createCommentLike']);
 
 $app->run();
